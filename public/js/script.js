d1b5e71c9e74337ebbacafec927e00bf27276329
GalvanLV <galvan.lopez@oclock.school>
2024-12-22 19:17:24 +0100

    homepage (mobile + tablet + laptop)

--- public/js/script.js
+++ public/js/script.js
@@ -4,39 +4,35 @@ const menuContent = document.querySelector('.menu__content');
 
 // menu transformation + button transformation
 menuButton.addEventListener('click', function() {
-    // if menu is expanded
+    // menu is expanded
     if (menu.classList.contains('expanded')) {
         // hide elements
         menuContent.style.transition = 'visibility 0s 0.2s, opacity 0.2s ease-out';
         menuContent.style.visibility = 'hidden';
         menuContent.style.opacity = '0';
 
-        // hide menu with delay
+        // hide menu
         setTimeout(function() {
             menu.classList.remove('expanded');
-            menuButton.classList.remove('open');
         }, 200);
-    
-    // if menu is compact
+
+        // change button
+        menuButton.textContent = 'Open menu';
+        menuButton.classList.remove('selected');
+    // menu is not expanded
     } else {
         // expand menu
         menu.classList.add('expanded');
 
+        // show elements
         setTimeout(function() {
-            // show elements with delay
             menuContent.style.transition = 'visibility 0s 0s, opacity 0.2s ease-in';
             menuContent.style.visibility = 'visible';
             menuContent.style.opacity = '1';
-        }, 300);
+        }, 200);
 
-        // change button color
-        menuButton.classList.add('open');
-    }
-
-    // Cambiar el texto del botón dependiendo del estado
-    if (menu.classList.contains('expanded')) {
-        menuButton.textContent = 'Close menu';  // Cuando el menú está abierto
-    } else {
-        menuButton.textContent = 'Open menu';  // Cuando el menú está cerrado
+        // change button
+        menuButton.textContent = 'Close menu';
+        menuButton.classList.add('selected');
     }
 });
