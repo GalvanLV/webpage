189fae13ada5e87161250fd23486044c876ffce9
GalvanLV <galvan.lopez@oclock.school>
2025-02-08 14:02:41 +0100

    info (about & skills)

--- public/js/submenuData.js
+++ public/js/submenuData.js
@@ -1,9 +1,67 @@
 export const contentData = {
     // info
-    about: '<h1>About Me</h1><p>This is the about section.</p>',
-    experience: '<h1>Experience</h1><p>This is the experience section.</p>',
-    skills: '<h1>Skills</h1><p>This is the skills section.</p>',
-    upcoming: '<h1>upcoming</h1><p>This is the upcoming section.</p>',
+    about: `
+        <img src="/images/profile.jpg" alt="profile image" class="about__image">
+        <p>Galvan Lopez<br><br><br>Fullstack Web Developer with a strong passion for design, known for my organized and structured approach. My journey into web development has been shaped by a unique blend of experiences in architecture, design, and technology.<br>I spent three years immersed in architecture school, where I developed a deep appreciation for structure, aesthetics, and problem-solving. This foundation was further enriched by a year in a design school, followed by another year specializing in game design and development. These diverse experiences laid the groundwork for my true passion: creating clean, functional, and visually appealing digital solutions.<br>My most significant milestone came during my time at O'clock School, where I honed my skills as a fullstack web developer. Here, I mastered the art of writing clean, efficient, and maintainable code—a reflection of my innate organizational skills and my belief that well-structured code is the backbone of great development.<br><br>While I strive for a balanced expertise in both front-end and back-end development, my heart leans towards design and the front-end. I love crafting user experiences that are not only functional but also visually engaging.<br>My work is driven by a desire to blend creativity with technical precision, ensuring that every project I undertake reflects both beauty and practicality.</p>
+        
+    `,
+    experience: `
+        <h1>Experience</h1><p>This is the experience section.</p>
+    `,
+    skills: `
+        <h2>FRONTEND DEVELOPMENT</h2>
+        <ul>
+            <li><strong>HTML5, CSS3 & Responsive Web Design:</strong> Modern and adaptive web interfaces.</li>
+            <li><strong>Advanced CSS:</strong> Styles, animations, and advanced transitions.</li>
+            <li><strong>JavaScript & DOM:</strong> Web interactivity, event handling, and form validation.</li>
+            <li><strong>Bootstrap:</strong> Responsive design and rapid prototyping.</li>
+            <li><strong>Github:</strong> Version control and collaboration in development environments.</li>
+        </ul><br>
+
+        <h2>BACKEND DEVELOPMENT</h2>
+        <ul>
+            <li><strong>PHP & OOP:</strong> Web application development with server logic and object-oriented programming.</li>
+            <li><strong>Form Management:</strong> Secure processing and validation of user data.</li>
+            <li><strong>CRUD & Web Security:</strong> Database management, security best practices, and permission handling.</li>
+            <li><strong>API & AJAX:</strong> Asynchronous interactions with servers.</li>
+        </ul><br>
+
+        <h2>DATABASES</h2>
+        <ul>
+            <li><strong>SQL & Relational Databases:</strong> Design, querying, and modeling of SQL databases.</li>
+            <li><strong>PDO (PHP Data Objects):</strong> Secure database connections with PDO in PHP.</li>
+            <li><strong>Project Management & DBMS:</strong> Database administration and collaboration on projects.</li>
+        </ul><br>
+
+        <h2>SOFTWARE ARCHITECTURE</h2>
+        <ul>
+            <li><strong>MVC:</strong> Design and development of web applications.</li>
+            <li><strong>PHP Dependencies & Composer:</strong> Library and dependency management.</li>
+            <li><strong>SEO & URL Rewriting:</strong> Web search engine optimization.</li>
+        </ul><br>
+
+        <h2>METHODOLOGIES & QUALITY CONTROL</h2>
+        <ul>
+            <li><strong>Agile Methods (Scrum/Kanban):</strong> Project management using agile methodologies.</li>
+            <li><strong>Code Quality Control</strong></li>
+        </ul><br>
+
+        <h2>SYSTEMS & CONTENT MANAGEMENT</h2>
+        <ul>
+            <li><strong>CMS (WordPress):</strong> Customization and management of web content.</li>
+            <li><strong>Hosting & Deployment:</strong> Environment configuration and project deployment.</li>
+        </ul><br>
+
+        <h2>BACKEND FRAMEWORKS</h2>
+        <ul>
+            <li><strong>Symfony:</strong> Web application development with the framework.</li>
+            <li><strong>Micro-frameworks:</strong> Development of lightweight and fast applications.</li>
+            <li><strong>ORM:</strong> Data mapping between the database and objects in the code.</li>
+        </ul>
+    `,
+    upcoming: `
+        <h1>upcoming</h1><p>This is the upcoming section.</p>
+    `,
     // projects
     origin: '<h1>O\'rigin</h1><p>This is the O\'rigin section.</p>',
     project__2: '<h1>project 2</h1><p>This is the project 2 section.</p>',
