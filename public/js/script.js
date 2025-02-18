6aed988086a0e2ba3610c2b595adb5f775cd5ed0
GalvanLV <galvan.lopez@oclock.school>
2025-02-18 11:36:50 +0100

    reconstruction (rename and version__old folder)

--- public/js/script.js
+++ public/js/script.js
@@ -1,103 +1,152 @@
-import { contentData } from './submenuData.js';
+const submenuData = {
+    'home': ['about me'],
+    'projects': ['origin', 'hasu production', 'Values', 'Careers'],
+    'info': ['Consulting', 'Development', 'Design', 'Support'],
+    'contact': ['Email', 'Phone', 'Address', 'Social Media']
+};
+
+const detailData = {
+    'about me': window.aboutMeData,
+    
+    'origin': window.originData,
+    'hasu production': ['Future Outlook', 'Innovation', 'Leadership Goals', 'Community Impact'],
+    'Values': ['Integrity', 'Excellence', 'Collaboration', 'Customer Focus'],
+    'Careers': ['Job Openings', 'Internships', 'Culture', 'Benefits'],
+
+    'Consulting': ['Strategy', 'Analysis', 'Planning', 'Execution'],
+    'Development': ['Web Development', 'Mobile Apps', 'Custom Solutions', 'Maintenance'],
+    'Design': ['UI/UX', 'Prototyping', 'Branding', 'Graphics'],
+    'Support': ['Technical Assistance', 'Customer Service', 'Knowledge Base', 'FAQs'],
+
+    'Email': ['General Inquiries', 'Support Contact', 'Media Contact', 'Feedback'],
+    'Phone': ['Main Line', 'Support Line', 'Sales Contact', 'Emergency Contact'],
+    'Address': ['Headquarters', 'Branches', 'PO Box', 'Directions'],
+    'Social Media': ['Facebook', 'Twitter', 'LinkedIn', 'Instagram']
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona el enlace "home" en el menú y le añade la clase "selected"
+    const homeLink = document.querySelector('[data-id="home"]');
+    homeLink.classList.add('selected');
+
+    // Asegura que el submenu para "home" se genera correctamente
+    submenuData['home'].forEach(submenuId => {
+        const item = document.createElement('div');
+        item.textContent = submenuId;
+        item.classList.add('submenu__link');
+        item.setAttribute('data-id', submenuId); // Establece el atributo 'data-id'
+        item.onclick = () => showDetail(null, submenuId);
+        submenu.appendChild(item);
+        
+        // Añade la clase 'selected' al submenu 'about me'
+    });
+
+    // Muestra el detalle de 'about me' automáticamente al cargar la página
+    const aboutMeLink = document.querySelector('[data-id="about me"]');
+    aboutMeLink.click();
+    aboutMeLink.classList.add('selected');
+});
+
+// item.onclick = () => showDetail(submenuId);
+// showDetail('about me');
 
 const menuButton = document.getElementById('menu__button');
 const menu = document.querySelector('.menu');
 const menuContent = document.querySelector('.menu__content');
 
-// menu transformation + button transformation
+                    //menu expansion + menu button modification
 menuButton.addEventListener('click', function() {
-    // menu is expanded
+    // if is expanded
     if (menu.classList.contains('expanded')) {
-        // hide elements
-        menuContent.style.transition = 'visibility 0s 0.2s, opacity 0.2s ease-out';
-        menuContent.style.visibility = 'hidden';
-        menuContent.style.opacity = '0';
-        // hide menu
+        menuContent.classList.toggle('expanded');
         setTimeout(function() {
-            menu.classList.remove('expanded');
+            menu.classList.toggle('expanded');
         }, 200);
-        // change button
-        menuButton.textContent = 'Open menu';
+        menuButton.textContent = 'open menu';
         menuButton.classList.remove('selected');
-    // menu is not expanded
+    // if not expanded
     } else {
-        // expand menu
-        menu.classList.add('expanded');
-        // show elements
+        menu.classList.toggle('expanded');
         setTimeout(function() {
-            menuContent.style.visibility = 'visible';
-            menuContent.style.opacity = '1';
+            menuContent.classList.toggle('expanded');
         }, 200);
-        // change button
-        menuButton.textContent = 'Close menu';
+        menuButton.textContent = 'close menu';
         menuButton.classList.add('selected');
     }
 });
 
-const submenuLinks = document.querySelectorAll('.submenu__link');
-const menuLinks = document.querySelectorAll('.menu__link');
-const submenu = document.querySelector('.submenu');
-const dynamicContent = document.getElementById('dynamicContent');
-const main = document.querySelector('.main');
+const submenu = document.getElementById('submenu');
+const detail = document.getElementById('detail');
+const wavesContainer = document.getElementById('waves__container');
 
-// submenu transition
-function handleContentTransition(link, contentKey) {
-    // start fade main
-    submenu.classList.add('change');
-    setTimeout(() => {
-        if (contentData[contentKey]) {
-            // update content
-            dynamicContent.innerHTML = contentData[contentKey] || '<h2>No Content</h2><p>Content not available.</p>';
-            // change "selected" link
-            submenuLinks.forEach(link => link.classList.remove('selected'));
-            link.classList.add('selected');
-            // show new content
-            dynamicContent.classList.add('change');
-        } else {
-            // if no matching content key, navigate to link href
-            window.location.href = link.getAttribute('href');
-        }
-        // hide/move submenu
-        submenu.classList.add('change');
-        dynamicContent.classList.add('change');
-    }, 200);
-    setTimeout(() => {
-        dynamicContent.classList.add('change');
-    }, 200);
+                    // submenu visible + menu link modification
+function showSubmenu(event, menuId) {   
+    // reset both submenu and detail
+    submenu.classList.remove('change');
+    detail.classList.remove('change');
+
+    detail.innerHTML = '';
+    submenu.innerHTML = '';
+    submenuData[menuId].forEach(submenuId => {
+        const item = document.createElement('div');
+        item.textContent = submenuId;
+        item.classList.add('submenu__link');
+        item.setAttribute('data-id', submenuId);
+        item.onclick = (event) => showDetail(event, submenuId);
+        submenu.appendChild(item);
+    });
+
+    // remove 'selected' from all menu links
+    document.querySelectorAll('.menu__link').forEach(link => {
+        link.classList.remove('selected');
+    });
+    // add 'selected' to current menu link
+    event.target.classList.add('selected');
+    
+    // close menu
+    menuButton.click();
 }
 
-const submenuDescriptions = document.querySelectorAll('.submenu__description');
+                    // detail visible + submenu link modification
+function showDetail(event, submenuId) {
+    submenu.classList.add('change');
+    detail.classList.add('change');
 
-// submenu links
-submenuLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const contentKey = this.getAttribute('data-content');
-        // hide all submenu__description
-        handleContentTransition(this, contentKey);
-        submenuDescriptions.forEach(description => {
-            description.classList.add('change');
-        });
-    });
-});
+    detail.innerHTML = '';   
 
-// menu links
-menuLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-        const contentKey = this.getAttribute('href').replace('.html', '');
-        // check if contentKey exists
-        if (contentData[contentKey]) {
-            e.preventDefault();
-            handleContentTransition(this, contentKey);
-        } else {
-            e.preventDefault();
-            // fade-out
-            main.classList.add('change');
-            // wait for fade-out
-            setTimeout(() => {
-                // go to .html
-                window.location.href = this.getAttribute('href');
-            }, 200);
-        }
+
+
+    // Crear el div con la clase 'detail__content'
+    const detailContent = document.createElement('div');
+    detailContent.classList.add('detail__content');
+
+
+
+    if (typeof detailData[submenuId] === 'string') {
+        // chain ---> HTML inyection
+        detailContent.innerHTML = detailData[submenuId];
+    } else {
+        // array ---> list
+        detailContent.innerHTML = detailData[submenuId]
+            .map(detailId => `<div>${detailId}</div>`)
+            .join('<br>');
+    }
+
+
+
+    detail.appendChild(detailContent); // Agregar el div al contenedor principal
+
+    // Forzar la animación de fade-in con un pequeño retraso
+    setTimeout(() => {
+        detailContent.classList.add('change'); // Activar la clase que hará el fade-in
+    }, 500);
+
+
+
+    // remove 'selected' from all submenu links
+    document.querySelectorAll('.submenu__link').forEach(link => {
+        link.classList.remove('selected');
     });
-});
+    // add 'selected' to current submenu link
+    event.target.classList.add('selected');
+}
---
+++ version__old/js/script.js
@@ -0,0 +1,103 @@
+import { contentData } from './submenuData.js';
+
+const menuButton = document.getElementById('menu__button');
+const menu = document.querySelector('.menu');
+const menuContent = document.querySelector('.menu__content');
+
+// menu transformation + button transformation
+menuButton.addEventListener('click', function() {
+    // menu is expanded
+    if (menu.classList.contains('expanded')) {
+        // hide elements
+        menuContent.style.transition = 'visibility 0s 0.2s, opacity 0.2s ease-out';
+        menuContent.style.visibility = 'hidden';
+        menuContent.style.opacity = '0';
+        // hide menu
+        setTimeout(function() {
+            menu.classList.remove('expanded');
+        }, 200);
+        // change button
+        menuButton.textContent = 'Open menu';
+        menuButton.classList.remove('selected');
+    // menu is not expanded
+    } else {
+        // expand menu
+        menu.classList.add('expanded');
+        // show elements
+        setTimeout(function() {
+            menuContent.style.visibility = 'visible';
+            menuContent.style.opacity = '1';
+        }, 200);
+        // change button
+        menuButton.textContent = 'Close menu';
+        menuButton.classList.add('selected');
+    }
+});
+
+const submenuLinks = document.querySelectorAll('.submenu__link');
+const menuLinks = document.querySelectorAll('.menu__link');
+const submenu = document.querySelector('.submenu');
+const dynamicContent = document.getElementById('dynamicContent');
+const main = document.querySelector('.main');
+
+// submenu transition
+function handleContentTransition(link, contentKey) {
+    // start fade main
+    submenu.classList.add('change');
+    setTimeout(() => {
+        if (contentData[contentKey]) {
+            // update content
+            dynamicContent.innerHTML = contentData[contentKey] || '<h2>No Content</h2><p>Content not available.</p>';
+            // change "selected" link
+            submenuLinks.forEach(link => link.classList.remove('selected'));
+            link.classList.add('selected');
+            // show new content
+            dynamicContent.classList.add('change');
+        } else {
+            // if no matching content key, navigate to link href
+            window.location.href = link.getAttribute('href');
+        }
+        // hide/move submenu
+        submenu.classList.add('change');
+        dynamicContent.classList.add('change');
+    }, 200);
+    setTimeout(() => {
+        dynamicContent.classList.add('change');
+    }, 200);
+}
+
+const submenuDescriptions = document.querySelectorAll('.submenu__description');
+
+// submenu links
+submenuLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const contentKey = this.getAttribute('data-content');
+        // hide all submenu__description
+        handleContentTransition(this, contentKey);
+        submenuDescriptions.forEach(description => {
+            description.classList.add('change');
+        });
+    });
+});
+
+// menu links
+menuLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        const contentKey = this.getAttribute('href').replace('.html', '');
+        // check if contentKey exists
+        if (contentData[contentKey]) {
+            e.preventDefault();
+            handleContentTransition(this, contentKey);
+        } else {
+            e.preventDefault();
+            // fade-out
+            main.classList.add('change');
+            // wait for fade-out
+            setTimeout(() => {
+                // go to .html
+                window.location.href = this.getAttribute('href');
+            }, 200);
+        }
+    });
+});
